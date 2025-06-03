805c00886dd26965eed33285a68cc648c6e64a2a
Jamite Ngola <jamitengola@hotmail.com>
2025-06-03 17:59:35 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+// server.js
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Serve arquivos estáticos da pasta atual
+app.use(express.static(__dirname));
+
+// Rota padrão para servir o index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
