a45d170e6540cef3eba15311f20c68fb65ca47d0
Jamite Ngola <jamitengola@hotmail.com>
2025-06-10 13:50:11 +0100

    atualizar porta do servidor para 3002 e ajustar a estrutura do flipbook no index.html

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3002;
 
 // Serve arquivos estáticos da pasta atual
 app.use(express.static(__dirname));
